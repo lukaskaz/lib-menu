f8449d0307d9264e8aa329f7d83dcf3cea965eea
Łukasz Kaźmierczak <lucas.kazmierczak@gmail.com>
2024-06-02 02:56:59 +0200

    Addding core files
    Signed-off-by: Łukasz Kaźmierczak <lucas.kazmierczak@gmail.com>

---
+++ inc/climenu.hpp
@@ -0,0 +1,29 @@
+#include <functional>
+#include <vector>
+
+using func = std::function<void()>;
+
+class Menu
+{
+  public:
+    Menu(const std::string& title,
+         std::vector<std::pair<std::string, func>>&& entries) :
+        title{title},
+        entries{std::move(entries)}
+    {}
+
+    void run() const;
+
+    static void waitenterpressed()
+    {
+        isenterpressed(-1);
+    }
+
+    static bool isenterpressed(int32_t timeoutMs = 0);
+
+  private:
+    const std::string title;
+    const std::vector<std::pair<std::string, func>> entries;
+
+    uint32_t getusersel() const;
+};
---
+++ src/climenu.cpp
@@ -0,0 +1,49 @@
+#include "climenu.hpp"
+
+#include <poll.h>
+
+#include <iostream>
+
+bool Menu::isenterpressed(int32_t timeoutMs)
+{
+    pollfd pollFd{fileno(stdin), POLLIN, 0};
+    if (0 < poll(&pollFd, 1, timeoutMs) && pollFd.revents & POLLIN)
+    {
+        while (getchar() != '\n')
+            ;
+        return true;
+    }
+    return false;
+}
+
+inline uint32_t Menu::getusersel() const
+{
+    uint32_t sel = getchar() - '0';
+    while ('\n' != getchar())
+        ;
+    return sel;
+}
+
+void Menu::run() const
+{
+    while (true)
+    {
+        uint32_t num{};
+
+        system("clear");
+        std::cout << "== " << title << " menu ==\n";
+        for (const auto& entry : this->entries)
+        {
+            const auto& info = std::get<std::string>(entry);
+            std::cout << ++num << " : to " << info << "\n";
+        }
+        std::cout << "-> ";
+
+        if (auto sel = getusersel() - 1; sel < num)
+        {
+            std::get<func>(this->entries.at(sel))();
+            std::cout << "Press enter to return to menu\n";
+            waitenterpressed();
+        }
+    }
+}
